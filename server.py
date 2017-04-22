34418fc0bf331b01c8a107fa51a639a410211d3c
shanelleroman <shanelle.roman@yale.edu>
2017-04-21 23:39:00 -0400

    added flask app

---
+++ server.py
@@ -0,0 +1,20 @@
+#!flask/bin/python
+from flask import Flask, jsonify, render_template, request, url_for, Response, make_response
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+	return "hello world!"
+	
+# takes client query, scrapes facebook group and returns json response
+@app.route('/query')
+def query():
+	return "hello"
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
