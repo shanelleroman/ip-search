1e3da957146a9da0587d0882c25fa7cd0efaf2ba
Shreya Dixit <shreya.dixit@yale.edu>
2017-04-23 14:53:25 -0400

    extra refernce files

--- fulltext.py
+++ fulltext.py
@@ -1,12 +1,13 @@
 #BUilding the database using SQL academy
 from flask import Flask 
 from flask_sqlalchemy import SQLAlchemy 
-from flask_whooshalchemy as wa
+import flask_whooshalchemy as wa
 
 app = Flask(__name__)
 
 #setting the congfig values
-appconfig['SQLALCHEMY_DATABASE_URI'] = 'mysql://prettypr_search:prettyprinted@prettyprinted.com/prettypr_whoosh'
+#appconfig['SQLALCHEMY_DATABASE_URI'] = 'mysql://prettypr_search:prettyprinted@prettyprinted.com/prettypr_whoosh'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///help.db'
 #really important: sql track modifications: allows whoosh alcehemy to know
 #when something changes in the database
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
@@ -21,10 +22,10 @@ db = SQLAlchemy(app)
 
 #creates a table 
 class Course(db.Model):
-	__searchable__ = [' name', 'description']
-	#three columns, one primary key
-	id= db.Column(db.Integer, primary_key=TRUE)
-	name = db.Column(db.Column(db.String(30)))
-	description = db.Column(db.String(100))
+	#defines which columns can be searched
+    __searchable__ = ['name', 'description']#three columns, one primary key
+    id= db.Column(db.Integer, primary_key=True)
+    name = db.Column((db.String(30)))
+    description = db.Column(db.String(100))
 
-wa.whoosh_index(app, Course) 
+wa.whoosh_index(app, Course)
